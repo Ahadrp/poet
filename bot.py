8947440cf9e5ce91820ebc6620df81772c4f49ee
Ahadrp <ahadrahimipour@gmail.com>
2024-01-06 21:46:24 +0000

    sending hello message every second --- not sent (issue #1)

--- bot.py
+++ bot.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 
 from dotenv import load_dotenv
+from telegram import Bot
+from telegram.error import TelegramError
 import requests
+import time
 import os
+
+def send_message(bot_token, channel_id, message):
+    try:
+        bot = Bot(token=bot_token)
+        print("Sending message...")
+        bot.send_message(chat_id=channel_id, text=message)
+    except TelegramError as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
 
     load_dotenv()
 
+    """
     url = "https://free-nba.p.rapidapi.com/players/237"
 
     headers = {
@@ -17,7 +29,18 @@ if __name__ == "__main__":
 
     response = requests.get(url, headers=headers)
 
-    # print(response.json())
+    print(response.json())
+    """
 
-    print(os.environ["TOKEN"])
+
+    # Set your bot token and channel ID
+    bot_token = os.environ["TOKEN"]
+    channel_id = os.environ["CHANNEL_ID"]
+
+    while True:
+        # Send the message
+        send_message(bot_token, channel_id, "Hello")
+
+        # Wait for one minute
+        time.sleep(1)
 
