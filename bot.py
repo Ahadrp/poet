ca2dafd7e8b5f05c664852d6f4137590c526a7f2
Ahadrp <ahadrahimipour@gmail.com>
2024-01-07 18:22:42 +0000

    Take response from api --- ok (issue #1).

--- bot.py
+++ bot.py
@@ -10,25 +10,46 @@ import time
 import os
 
 logging.basicConfig(
-    level=logging.DEBUG,  # Set the desired logging level
+    level=logging.INFO,  # Set the desired logging level
     format="%(asctime)s [%(levelname)s] %(message)s",  # Define the log message format
     datefmt="%Y-%m-%d %H:%M:%S"  # Define the date format
 )
 
-async def send_message(bot_token, chat_id, message):
+async def send_req() -> str:
+    response = ""
+    url = "https://free-nba.p.rapidapi.com/players/237"
+
+    headers = {
+        "X-RapidAPI-Key": "eb12b991e4msh8390ec23a48d4f4p1054d1jsne0419ea7b577",
+        "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
+    }
+
     try:
-        bot = Bot(token=bot_token)
+        response = requests.get(url, headers=headers)
+        logger.info(f"Response '{response}'has been taken successfully!")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"sending req failed because: '{e}'")
+        return
+
+    return response.json()
+
+
+async def send_message(bot_token, chat_id, message):
+    resp = ""
+    bot = Bot(token=bot_token)
+
+    try:
         await bot.send_message(chat_id=chat_id, text=message)
-        logger.debug(f"Message '{message}' has been send successfully")
+        logger.info(f"Message '{message}' has been send successfully")
     except TelegramError as e:
         logger.error(f"Error while sending message '{message}' because: {e}")
 
 async def send_hello_periodically(bot_token, chat_id, interval_seconds):
-    logger.debug(f"call hello method peridically")
+    logger.info(f"call hello method peridically")
 
     while True:
-        # resp = send_req()
-        await send_message(bot_token, chat_id, "Hello")
+        resp = await send_req()
+        await send_message(bot_token, chat_id, resp)
         await asyncio.sleep(interval_seconds)
 
 if __name__ == "__main__":
@@ -37,23 +58,10 @@ if __name__ == "__main__":
     load_dotenv()
     logger.info("env has been loaded")
 
-    """
-    url = "https://free-nba.p.rapidapi.com/players/237"
-
-    headers = {
-        "X-RapidAPI-Key": "eb12b991e4msh8390ec23a48d4f4p1054d1jsne0419ea7b577",
-        "X-RapidAPI-Host": "free-nba.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    print(response.json())
-    """
-
     # Set your bot token and channel ID
     bot_token = os.environ["TOKEN"]
     channel_id = os.environ["CHANNEL_ID"]
-    logger.debug(f"token '{bot_token}' and channel id '{channel_id}' has been taken")
+    logger.info(f"token '{bot_token}' and channel id '{channel_id}' has been taken")
     interval_seconds = 3
 
     loop = asyncio.get_event_loop()
